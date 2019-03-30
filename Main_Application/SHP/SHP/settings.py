09703cb6c5897e119da8a60bd85cd82ed6a9d3ae
Roger Ho <pingche2@illinois.edu>
2019-03-29 21:37:13 -0500

    Added requirements.txt for new CORS django module and middleware

--- Main_Application/SHP/SHP/settings.py
+++ Main_Application/SHP/SHP/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backpack',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }

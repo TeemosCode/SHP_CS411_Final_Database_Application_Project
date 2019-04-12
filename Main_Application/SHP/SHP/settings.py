2d6c4b6b82fd86647d27461231c39184ededd309
NeoWu1216 <neo1216.wu@gmail.com>
2019-04-12 13:29:40 -0500

    Allow different password configurations

--- Main_Application/SHP/SHP/settings.py
+++ Main_Application/SHP/SHP/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from .password import PASSWORD
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backpack',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': PASSWORD,
         'HOST': 'localhost',
         'PORT': '3306',
     }

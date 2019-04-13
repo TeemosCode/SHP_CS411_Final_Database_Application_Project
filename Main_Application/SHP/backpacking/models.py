606a4b994e6df7ac6af9d0536a675f52926e9a3d
Roger Ho <pingche2@illinois.edu>
2019-04-12 22:07:37 -0500

    Changed BUser table, adding in facebook_user_id field

--- Main_Application/SHP/backpacking/models.py
+++ Main_Application/SHP/backpacking/models.py
@@ -11,16 +11,13 @@ from django.db import models
 class Buser(models.Model):
     userid = models.AutoField(primary_key=True)
     open_match = models.IntegerField(blank=True, null=True)
-    username = models.CharField(
-        unique=True, max_length=50, blank=True, null=True)
+    username = models.CharField(unique=True, max_length=50, blank=True, null=True)
     profile_pic = models.CharField(max_length=200, blank=True, null=True)
     firstname = models.CharField(max_length=50, blank=True, null=True)
     lastname = models.CharField(max_length=50, blank=True, null=True)
-    email = models.CharField(max_length=50, blank=True, null=True)
+    email = models.CharField(unique=True, max_length=50, blank=True, null=True)
     info = models.CharField(max_length=10000, blank=True, null=True)
-
-    def __str__(self):
-        return "%s - %s" % (self.userid, self.username)
+    facebook_user_id = models.CharField(unique=True, max_length=128, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -30,8 +27,7 @@ class Buser(models.Model):
 class Blogphoto(models.Model):
     photoid = models.AutoField(primary_key=True)
     photo_url = models.CharField(max_length=100, blank=True, null=True)
-    postid = models.ForeignKey(
-        'Blogpost', models.DO_NOTHING, db_column='postid')
+    postid = models.ForeignKey('Blogpost', models.DO_NOTHING, db_column='postid')
 
     class Meta:
         managed = False
@@ -42,15 +38,8 @@ class Blogpost(models.Model):
     postid = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, blank=True, null=True)
     content = models.CharField(max_length=10000, blank=True, null=True)
-    # create_time should be set to current time by default
-    # refer to https://medium.com/django-musings/current-datetime-for-default-value-in-a-model-use-callables-85e092e68fa6
-    # not update yet
-    create_time = models.DateTimeField(blank=True, null=True)
-    author = models.ForeignKey(
-        Buser, models.DO_NOTHING, db_column='author', blank=True, null=True)
-
-    def __str__(self):
-        return "%s - %s" % (self.postid, self.title)
+    create_time = models.DateTimeField()
+    author = models.ForeignKey(Buser, models.DO_NOTHING, db_column='author', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -58,8 +47,7 @@ class Blogpost(models.Model):
 
 
 class Blogtag(models.Model):
-    postid = models.ForeignKey(
-        Blogpost, models.DO_NOTHING, db_column='postid', primary_key=True)
+    postid = models.ForeignKey(Blogpost, models.DO_NOTHING, db_column='postid', primary_key=True)
     tagid = models.ForeignKey('Tag', models.DO_NOTHING, db_column='tagid')
 
     class Meta:
@@ -72,14 +60,9 @@ class Comment(models.Model):
     commentid = models.AutoField(primary_key=True)
     comment_time = models.DateTimeField(blank=True, null=True)
     content = models.CharField(max_length=5000, blank=True, null=True)
-    postid = models.ForeignKey(
-        Blogpost, models.DO_NOTHING, db_column='postid', blank=True, null=True)
+    postid = models.ForeignKey(Blogpost, models.DO_NOTHING, db_column='postid', blank=True, null=True)
     userid = models.ForeignKey(Buser, models.DO_NOTHING, db_column='userid')
-    parentid = models.ForeignKey(
-        'self', models.DO_NOTHING, db_column='parentid', blank=True, null=True)
-
-    def __str__(self):
-        return "%s - %s" % (self.commentid, self.postid)
+    parentid = models.ForeignKey('self', models.DO_NOTHING, db_column='parentid', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -87,8 +70,7 @@ class Comment(models.Model):
 
 
 class Likepost(models.Model):
-    userid = models.ForeignKey(
-        Buser, models.DO_NOTHING, db_column='userid', primary_key=True)
+    userid = models.ForeignKey(Buser, models.DO_NOTHING, db_column='userid', primary_key=True)
     postid = models.ForeignKey(Blogpost, models.DO_NOTHING, db_column='postid')
 
     class Meta:
@@ -111,22 +93,12 @@ class Tag(models.Model):
 class Travelinfo(models.Model):
     travelinfo_id = models.AutoField(primary_key=True)
     activity = models.CharField(max_length=200, blank=True, null=True)
-    # Field name made lowercase.
-    budgetmax = models.IntegerField(
-        db_column='budgetMax', blank=True, null=True)
-    # Field name made lowercase.
-    budgetmin = models.IntegerField(
-        db_column='budgetMin', blank=True, null=True)
+    budgetmax = models.IntegerField(db_column='budgetMax', blank=True, null=True)  # Field name made lowercase.
+    budgetmin = models.IntegerField(db_column='budgetMin', blank=True, null=True)  # Field name made lowercase.
     destination = models.CharField(max_length=100, blank=True, null=True)
-    # Field name made lowercase.
-    starttime = models.DateField(db_column='startTime', blank=True, null=True)
-    # Field name made lowercase.
-    endtime = models.DateField(db_column='endTime', blank=True, null=True)
-    userid = models.ForeignKey(
-        Buser, models.DO_NOTHING, db_column='userid', blank=True, null=True)
-
-    def __str__(self):
-        return "%s - %s" % (self.travelinfo_id, self.userid)
+    starttime = models.DateField(db_column='startTime', blank=True, null=True)  # Field name made lowercase.
+    endtime = models.DateField(db_column='endTime', blank=True, null=True)  # Field name made lowercase.
+    userid = models.ForeignKey(Buser, models.DO_NOTHING, db_column='userid', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -134,8 +106,7 @@ class Travelinfo(models.Model):
 
 
 class Usertag(models.Model):
-    userid = models.ForeignKey(
-        Buser, models.DO_NOTHING, db_column='userid', primary_key=True)
+    userid = models.ForeignKey(Buser, models.DO_NOTHING, db_column='userid', primary_key=True)
     tagid = models.ForeignKey(Tag, models.DO_NOTHING, db_column='tagid')
 
     class Meta:

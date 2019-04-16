fe6c269c2f89733931f3ab790da35da853bf3f05
shuyanl3 <34068998+shuyanl3@users.noreply.github.com>
2019-04-16 17:22:25 -0500

    Update settings.py

--- Main_Application/SHP/SHP/settings.py
+++ Main_Application/SHP/SHP/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backpack',
         'USER': 'root',
-        'PASSWORD': "123456",
+        'PASSWORD': PASSWORD,
         'HOST': 'localhost',
         'PORT': '3306',
     }
